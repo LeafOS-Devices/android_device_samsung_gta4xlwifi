2c8899b0d46baf1f092e92d94127f9d5fbb8cd3b
Tim Zimmermann <tim@linux4.de>
2024-07-06 15:40:17 +0200

    gta4xlwifi: Adapt for leaf
    Change-Id: Ia0266b23932c463d00f9ef244cbdc2e932702907

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gta4xlwifi.mk
+    $(LOCAL_DIR)/gta4xlwifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gta4xlwifi-eng \
-    lineage_gta4xlwifi-user \
-    lineage_gta4xlwifi-userdebug
+    gta4xlwifi-eng \
+    gta4xlwifi-user \
+    gta4xlwifi-userdebug
